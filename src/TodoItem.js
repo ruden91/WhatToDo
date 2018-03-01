0bf0017578eb4c9aab0e7e5dea77c3b529ca7da2
ruden91 <ruden91@naver.com>
2018-03-01 21:03:10 +0900

    remove todoItem event 추가

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleTodoItem = this.handleTodoItem.bind(this);
+  }
+  handleTodoItem() {
+    console.log('double click');
+  }  
   render() {
-    const { item } = this.props;
+    const { index, item, removeTodoItem, updateTodoItem } = this.props;
     return (
       <li>
-        { item.text }
+        <button onClick={ () => updateTodoItem(index) }>업데이트</button>
+        <p onDoubleClick={ this.handleTodoItem }>{ item.text }</p>
+        <button onClick={() => removeTodoItem(index)}>삭제</button>
       </li>
     )
   }
