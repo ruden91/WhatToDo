87ba272a7403a740cf2cb412c7be9ff8a50f96bd
ruden91 <ruden91@naver.com>
2018-03-03 13:44:44 +0900

    sidebar menu 컴포넌트 구현완료

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-// import Login from './Login';
-import UserInfo from 'components/UserInfo';
-
 export default class AppHeader extends Component {
   constructor(props) {
     super(props);
@@ -13,10 +10,6 @@ export default class AppHeader extends Component {
 
     return (
       <header className="todo-app-header">
-        <button className="todo-app-header__toggle-side-menu">
-          <i className="fas fa-bars"></i>
-        </button>
-        { currentUser && <UserInfo currentUser={ currentUser } /> }
         <button className="todo-app-header__show-add-button">
           <i className="fas fa-plus-circle"></i>
         </button>
--- src/components/BoardMain.js
+++ src/components/BoardMain.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 
 import AppHeader from 'components/AppHeader';
 import TodoBoard from 'containers/TodoBoard';
+import { elastic as Menu } from 'react-burger-menu';
 
+import UserInfo from 'components/UserInfo';
 export default class BoardMain extends Component {
   render() {
     const { todoItems, currentUser } = this.props;  
     return (
-      <div> 
-        <AppHeader currentUser={ currentUser }/>
-        <div className="todo-app__content">
-          { <TodoBoard todoItems={ todoItems } currentUser={ currentUser } /> }
+      <div id="outer-container"> 
+        <Menu 
+          pageWrapId={ "page-wrap" } 
+          outerContainerId={ "outer-container" } 
+          width={ 240 } 
+          disableOverlayClick 
+        >
+          { currentUser && <UserInfo currentUser={ currentUser } /> }
+        </Menu>
+        <div id="page-wrap">
+          <AppHeader currentUser={ currentUser }/>
+          <div className="todo-app__content">
+            { <TodoBoard todoItems={ todoItems } currentUser={ currentUser } /> }
+          </div>          
         </div>
       </div>
     )
