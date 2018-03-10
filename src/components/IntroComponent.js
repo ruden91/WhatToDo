00e27c93b471291866020f615ddd0564f555c28b
ruden <ruden91@mycelebs.com>
2018-03-10 19:12:41 +0900

    brand intro component 생성

--- src/components/IntroComponent.js
+++ src/components/IntroComponent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import BrandIntro from 'components/intro/BrandIntro';
 import AboutDeveloper from 'components/intro/AboutDeveloper';
 const IntroComponent = () => (
   <div>
+    <BrandIntro />
     <AboutDeveloper />
   </div>
 )
---
+++ src/components/intro/BrandIntro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BrandIntro = () => (
+  <div className="wtd-brand-intro">
+    <p>WhatToDo와 함께 일정관리를 시작하세요</p>
+    <a href="#">시작하세요 - 무료입니다</a>
+  </div>
+)
+
+export default BrandIntro;
