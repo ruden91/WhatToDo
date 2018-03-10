0a4fbd16bd81f71e3dba591ffab13183f234dfc1
ruden91 <ruden91@naver.com>
2018-03-11 01:32:19 +0900

    [수정] signup style

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,8 @@ import AppHeader from 'components/AppHeader';
 import IntroComponent from 'components/IntroComponent';
 import AppFooter from 'components/AppFooter';
 
-import Modal from 'react-modal';
+import ReactModal from 'react-modal';
+
 export default class App extends Component {
   state = {
     toggleModal: false
@@ -73,16 +74,50 @@ export default class App extends Component {
         <IntroComponent handleSignUpButton={ this.handleSignUpButton } />
         <AppFooter />
 
-        <Modal
+        <ReactModal
           isOpen={ toggleModal }
           onRequestClose={this.handleSignUpModalClose}
           ariaHideApp={ false }
           contentLabel="loginModal"
           style={ customStyles }
+          closeTimeoutMS={200}
         >
-          modal hello
-          <button onClick={ this.handleSignUpModalClose }>close</button>
-        </Modal>
+          <div className="wtd-signup-modal__inner-container">
+            <header className="wtd-signup-modal__header">
+              <p>바로 가입</p>
+              <span className="wtd-signup-modal__close-btn-wrap">
+                <button onClick={ this.handleSignUpModalClose }>
+                </button>              
+              </span>
+            </header>
+            <div className="wtd-signup-modal__content">
+              <div className="wtd-signup-modal__separator">
+                <span>또는</span>
+              </div>
+              <form className="wtd-signup-modal__signup-form">
+                <label htmlFor="signup-modal-nickname">
+                  <i className="fas fa-user"></i>
+                  <input 
+                    type="text" 
+                    placeholder="당신의 닉네임" 
+                    title="당신의 닉네임" 
+                    id="signup-modal-nickname"
+                    maxLength="10"
+                  />
+                </label>
+                <label htmlFor="signup-modal-email">
+                  <i className="far fa-envelope"></i>
+                  <input type="email" placeholder="이메일" title="이메일" id="signup-modal-email" maxLength="30"/>
+                </label>
+                <label htmlFor="signup-modal-password">
+                  <i className="fas fa-unlock"></i>
+                  <input type="password" placeholder="패스워드" title="패스워드" id="signup-modal-password" />
+                </label>
+                <input type="submit" value="내 계정 생성" />
+              </form>
+            </div>          
+          </div>
+        </ReactModal>
       </div>
     )
   }
