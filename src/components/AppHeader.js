7a51ed348e17c3cbc112ef09d2467d1805b61ada
ruden91 <ruden91@naver.com>
2018-03-10 17:45:12 +0900

    AppHeader 리팩토링 및 서비스 네이밍 변경

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,36 +1,82 @@
 import React, { Component } from 'react';
 
-export default class AppHeader extends Component {
-  constructor(props) {
-    super(props);
-  }
+const AppHeader = () => (
+  <header className="wtd-header">
+    <div className="wtd-header__floating">
+      <div className="wtd-container">
+        <div className="wtd-header-inner">
+          <div className="wtd-header__brand">
+            <a href="#" className="wtd-header__logo-holder">
+              WhatToDo
+            </a>
+          </div>
+          <div className="wtd-header__actions-holder">
+            <ul className="wtd-header__actions">
+              <li className="wtd-header__action">
+                <a href="#" className="wtd-header__action-link">
+                  설명
+                </a>
+              </li>  
+              <li className="wtd-header__action">
+                <a href="#" className="wtd-header__action-link">
+                  프리미엄
+                </a>
+              </li>
+              <li className="wtd-header__action">
+                <a href="#" className="wtd-header__action-link">
+                  비즈니스
+                </a>
+              </li>                                      
+              <li className="wtd-header__action">
+                <a href="#" className="wtd-header__action-link">
+                  로그인
+                </a>
+              </li>
+              <li className="wtd-header__action">
+                <a href="#" className="wtd-header__action-link">
+                  가입
+                </a>
+              </li>              
+            </ul>
+          </div>
+        </div>
+      </div>    
+    </div>
+  </header>
+)
 
-  render() {
-    const style = {
-      backgroundColor: this.props.settings ? this.props.settings.backgroundColor : ""
-    }
+export default AppHeader;
+// export default class AppHeader extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-    const writeButtonStyle = {
-      '&::before': this.props.settings ? this.props.settings.backgroundColor : "",
-      '&::after': this.props.settings ? this.props.settings.backgroundColor : ""
-    }
-    const { toggleAddTodoItem, toggleAddTodoItemButton } = this.props;
+//   render() {
+//     // const style = {
+//     //   backgroundColor: this.props.settings ? this.props.settings.backgroundColor : ""
+//     // }
 
-    return (
-      <header className="todo-app-header" style={style}>
-        <span className="todo-app-header__header-logo">
-          <i className="fas fa-list-ol"></i>
-        </span>
-        <button 
-          className="todo-app-header__add-button-toggle js-todo-app-header__add-button-toggle"
-          onClick={ () => toggleAddTodoItem() }
-        >
-          <span className={toggleAddTodoItemButton ? 'open' : 'close'}>
-            <span style={ style }></span>
-            <span style={ style }></span>
-          </span>
-        </button>
-      </header>
-    )
-  }
-}
+//     // const writeButtonStyle = {
+//     //   '&::before': this.props.settings ? this.props.settings.backgroundColor : "",
+//     //   '&::after': this.props.settings ? this.props.settings.backgroundColor : ""
+//     // }
+//     // const { toggleAddTodoItem, toggleAddTodoItemButton } = this.props;
+
+//     return (
+//       <header className="todo-app-header" style={style}>
+//         <span className="todo-app-header__header-logo">
+//           <i className="fas fa-list-ol"></i>
+//         </span>
+//         <button 
+//           className="todo-app-header__add-button-toggle js-todo-app-header__add-button-toggle"
+//           onClick={ () => toggleAddTodoItem() }
+//         >
+//           <span className={toggleAddTodoItemButton ? 'open' : 'close'}>
+//             <span style={ style }></span>
+//             <span style={ style }></span>
+//           </span>
+//         </button>
+//       </header>
+//     )
+//   }
+// }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,125 +1,137 @@
 import React, { Component } from 'react';
-import { database, auth } from 'database/firebase';
 
-import MainLoading from 'components/MainLoading';
-import Dashboard from 'components/Dashboard';
-import Main from 'containers/Main';
-import { map, omit } from 'lodash';
-
-import LoginForm from 'containers/LoginForm';
-import Signup from 'containers/Signup';
-import $ from 'jquery';
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      todoItems: null,
-      currentUser: null,
-      loading: true,
-      toggleAddTodoItemButton: false,
-      settings: null,
-      messages: null,
-      verified: false
-    }   
-
-    this.toggleAddTodoItem = this.toggleAddTodoItem.bind(this);
-  }
-
-  toggleAddTodoItem() {
-    this.setState({
-      toggleAddTodoItemButton: !this.state.toggleAddTodoItemButton
-    })
-  }
-
-  componentDidMount() {
-    // fetch userData and user todoItems
-    auth.onAuthStateChanged(currentUser => {
-      this.setState({
-        currentUser,
-        loading: true
-      })
-      if (currentUser) {
-        // email 인증 유무 체크
-        if (currentUser.emailVerified) {
-          if (!currentUser.displayName) {
-            currentUser.updateProfile({
-              displayName: '흑미밥',
-              photoURL: 'https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-1/c14.0.48.48/p48x48/10354686_10150004552801856_220367501106153455_n.jpg?oh=26830885109cf41da057908f11c5ec33&oe=5B0FF559'
-            })
-          }
-          this.setState({
-            verified: true
-          })
-        } else {
-          alert('본인 확인을 위해 이메일 인증을 해주세요.');
-          auth.signOut();
-        }
-        // // user defaultSettings
-        database.ref('settings/' + currentUser.uid).on('value', (snap) => {
-          if (snap.val()) {
-            $('meta[name=theme-color]').attr('content', snap.val().backgroundColor);
-          }
-          this.setState({
-            settings: snap.val()
-          })
-        })
-
-        // messages data
-        database.ref('messages').on('value', (snap) => {
-          this.setState({
-            messages: snap.val()
-          })          
-        })
-
-        // user todoItems
-        database.ref('todoItems/' + currentUser.uid).on('value', (snap) => {
-          this.setState({
-            todoItems: this.sortTodoItems(snap.val()),
-            loading: false
-          })
-        })
-      } else {
-        this.setState({
-          todoItems: null,
-          loading: false
-        })        
-      }  
-    })
-  }
-  // 최신순으로 todoItems를 정렬해주는 함수
-  sortTodoItems(items) {
-    let results = {};
-    let tempArray = map(items, (value, key) => ({ ...value, key })).reverse();
-
-    map(tempArray, (value) => results[value.key] = omit(value, 'key'));
-
-    return results;
-  }
+import AppHeader from 'components/AppHeader';
+export default class App extends Component {
   render() {
-    const { currentUser, loading, verified } = this.state;
-    let template;
-
-    if (currentUser) {
-      if (verified) {
-        // dashboard 컴포넌트
-        template = <Dashboard { ...this.state } toggleAddTodoItem={ this.toggleAddTodoItem } />
-      } else {
-        // 이메일 인증 문구와 함께 첫화면 컴포넌트
-        template = <LoginForm />
-      }
-    } else {
-        // 첫화면 컴포넌트
-        template = <LoginForm />
-    }
-
     return (
-      <div className="todo-app">
-        {loading && <MainLoading />}
-        {!loading && template }
+      <div className="wtd">
+        <AppHeader />
       </div>
     )
   }
 }
+// import React, { Component } from 'react';
+// import { database, auth } from 'database/firebase';
 
-export default App;
+// import MainLoading from 'components/MainLoading';
+// import Dashboard from 'components/Dashboard';
+// import Main from 'containers/Main';
+// import { map, omit } from 'lodash';
+
+// import LoginForm from 'containers/LoginForm';
+// import Signup from 'containers/Signup';
+// import $ from 'jquery';
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       todoItems: null,
+//       currentUser: null,
+//       loading: true,
+//       toggleAddTodoItemButton: false,
+//       settings: null,
+//       messages: null,
+//       verified: false
+//     }   
+
+//     this.toggleAddTodoItem = this.toggleAddTodoItem.bind(this);
+//   }
+
+//   toggleAddTodoItem() {
+//     this.setState({
+//       toggleAddTodoItemButton: !this.state.toggleAddTodoItemButton
+//     })
+//   }
+
+//   componentDidMount() {
+//     // fetch userData and user todoItems
+//     auth.onAuthStateChanged(currentUser => {
+//       this.setState({
+//         currentUser,
+//         loading: true
+//       })
+//       if (currentUser) {
+//         // email 인증 유무 체크
+//         if (currentUser.emailVerified) {
+//           if (!currentUser.displayName) {
+//             currentUser.updateProfile({
+//               displayName: '흑미밥',
+//               photoURL: 'https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-1/c14.0.48.48/p48x48/10354686_10150004552801856_220367501106153455_n.jpg?oh=26830885109cf41da057908f11c5ec33&oe=5B0FF559'
+//             })
+//           }
+//           this.setState({
+//             verified: true
+//           })
+//         } else {
+//           alert('본인 확인을 위해 이메일 인증을 해주세요.');
+//           auth.signOut();
+//         }
+//         // // user defaultSettings
+//         database.ref('settings/' + currentUser.uid).on('value', (snap) => {
+//           if (snap.val()) {
+//             $('meta[name=theme-color]').attr('content', snap.val().backgroundColor);
+//           }
+//           this.setState({
+//             settings: snap.val()
+//           })
+//         })
+
+//         // messages data
+//         database.ref('messages').on('value', (snap) => {
+//           this.setState({
+//             messages: snap.val()
+//           })          
+//         })
+
+//         // user todoItems
+//         database.ref('todoItems/' + currentUser.uid).on('value', (snap) => {
+//           this.setState({
+//             todoItems: this.sortTodoItems(snap.val()),
+//             loading: false
+//           })
+//         })
+//       } else {
+//         this.setState({
+//           todoItems: null,
+//           loading: false
+//         })        
+//       }  
+//     })
+//   }
+//   // 최신순으로 todoItems를 정렬해주는 함수
+//   sortTodoItems(items) {
+//     let results = {};
+//     let tempArray = map(items, (value, key) => ({ ...value, key })).reverse();
+
+//     map(tempArray, (value) => results[value.key] = omit(value, 'key'));
+
+//     return results;
+//   }
+//   render() {
+//     const { currentUser, loading, verified } = this.state;
+//     let template;
+
+//     if (currentUser) {
+//       if (verified) {
+//         // dashboard 컴포넌트
+//         template = <Dashboard { ...this.state } toggleAddTodoItem={ this.toggleAddTodoItem } />
+//       } else {
+//         // 이메일 인증 문구와 함께 첫화면 컴포넌트
+//         template = <LoginForm />
+//       }
+//     } else {
+//         // 첫화면 컴포넌트
+//         template = <LoginForm />
+//     }
+
+//     return (
+//       <div className="todo-app">
+//         {loading && <MainLoading />}
+//         {!loading && template }
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
