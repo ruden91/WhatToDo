929efb734918a4c386f8096a6b52ca0aacf5cc8d
ruden <ruden91@mycelebs.com>
2018-03-10 19:02:49 +0900

    site main intro module 생성

---
+++ src/components/IntroComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import AboutDeveloper from 'components/intro/AboutDeveloper';
+const IntroComponent = () => (
+  <div>
+    <AboutDeveloper />
+  </div>
+)
+
+export default IntroComponent;
