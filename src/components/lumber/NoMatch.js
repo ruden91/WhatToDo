ef78ddf8a9993ecf3aa35f7fb7832c4680cea084
ruden <ruden91@mycelebs.com>
2018-03-10 21:43:06 +0900

    [수정] 404 page component 스타일

--- src/components/lumber/NoMatch.js
+++ src/components/lumber/NoMatch.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 const NoMatch = () => (
-  <div>
-    NoMatch Component
+  <div className="wtd-no-match">
+    <p className="wtd-no-match__logo">
+      <Link to="/">WhatToDo LOGO</Link>
+    </p>
+    <h3><b>404</b> Sorry, the page you're looking for isn’t here anymore</h3>
+    <p className="wtd-no-match__support">
+      If you reached this page from another part of <Link to="/">WhatToDo App</Link>, 
+      please <Link to="support">let us know</Link> so we can correct our mistake.
+    </p>
   </div>
 )
 
