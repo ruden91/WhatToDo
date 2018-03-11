4adbfc0c07d19989f588948be68e79d57bc0c694
ruden <ruden91@mycelebs.com>
2018-03-11 15:56:08 +0900

    [추가] DashboardAsideMenu 컴포넌트 사이드 패널 목록 퍼블리싱

--- src/components/dashboard/DashboardAsideMenu.js
+++ src/components/dashboard/DashboardAsideMenu.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const DashboardAsideMenu = () => (
   <aside className="wtd-dashboard-aside-menu">
-    DashboardAsideMenu
+    <ul className="wtd-dashboard-aside-menu__panels">
+      <li className="wtd-dashboard-aside-menu__panel current">
+        <span className="wtd-dashboard-aside-menu__panel-icon"><i className="fas fa-inbox"></i></span>
+        <span className="wtd-dashboard-aside-menu__panel-title">관리함</span> <small>7</small>
+      </li>
+      <li className="wtd-dashboard-aside-menu__panel">
+        <span className="wtd-dashboard-aside-menu__panel-icon"><i className="far fa-calendar"></i></span>
+        <span className="wtd-dashboard-aside-menu__panel-title">오늘</span> <small>3</small>
+      </li>
+      <li className="wtd-dashboard-aside-menu__panel">
+        <span className="wtd-dashboard-aside-menu__panel-icon"><i className="fas fa-calendar-alt"></i></span>
+        <span className="wtd-dashboard-aside-menu__panel-title">다음 7일</span> <small>1</small>
+      </li>
+    </ul>
   </aside>
 )
 
