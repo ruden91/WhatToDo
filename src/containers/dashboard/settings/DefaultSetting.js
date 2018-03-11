68cda1fcc5d68c26800772ae293bfb9d91a74783
ruden91 <ruden91@naver.com>
2018-03-11 17:49:52 +0900

    [추가] setting conditional component

---
+++ src/containers/dashboard/settings/DefaultSetting.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class DefaultSetting extends Component {
+  render() {
+    return (
+      <div>
+        DefaultSetting
+      </div>
+    )
+  }
+}
