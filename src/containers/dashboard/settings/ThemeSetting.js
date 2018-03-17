9a65349eabaaefedcfef46a5414702020f21c5f1
ruden <ruden91@mycelebs.com>
2018-03-17 14:41:37 +0900

    ReadMe 뱃지추가

--- src/containers/dashboard/settings/ThemeSetting.js
+++ src/containers/dashboard/settings/ThemeSetting.js
@@ -3,8 +3,80 @@ import React, { Component } from 'react';
 export default class ThemeSetting extends Component {
   render() {
     return (
-      <div>
-        z
+      <div className="wtd-dashboard-theme-setting">
+        <p>테마</p>
+        
+        <div>
+          <p>색상 선택</p>
+          <ul>
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>                                                                                    
+            <li>
+              <span></span>
+              <span>WhatToDo</span>
+            </li>            
+          </ul>
+
+          <div className="wtd-dashboard-theme-setting__monitor-container">
+            <header>
+
+            </header>
+
+            <aside>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+            </aside>
+            <div>
+              <span className="line"></span>
+              <span className="circle-and-line"></span>
+              <span className="circle-and-line"></span>
+              <span className="circle-and-line"></span>
+              <span className="circle-and-line"></span>
+              <span className="circle-and-line"></span>
+              <span className="cross"></span>
+            </div>
+          </div>          
+        </div>
       </div>
     )
   }
