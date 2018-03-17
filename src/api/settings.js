d998bda788110b993c9e1812e41c808799442764
ruden91 <ruden91@naver.com>
2018-03-17 15:26:22 +0900

    [수정] Theme Setting component

---
+++ src/api/settings.js
@@ -0,0 +1,54 @@
+export const themes = () => {
+  return [
+  {
+    name: 'WhatToDo',
+    color: '#db4c3f',
+    active: false
+  },
+  {
+    name: '누아르',
+    color: '#3d3d3d',
+    active: false
+  },
+  {
+    name: '뉴트럴',
+    color: '#f7f7f7',
+    active: false
+  },
+  {
+    name: '탄제린',
+    color: '#ff9000',
+    active: false
+  },
+  {
+    name: '선플라워',
+    color: '#fecf05',
+    active: false
+  },
+  {
+    name: '클로버',
+    color: '#3c9b0d',
+    active: false
+  },
+  {
+    name: '블루베리',
+    color: '#316fea',
+    active: false
+  },    
+  {
+    name: '스카이',
+    color: '#4dc1e6',
+    active: false
+  },    
+  {
+    name: '애머시스트',
+    color: '#8e24aa',
+    active: false
+  },      
+  {
+    name: '그라파이트',
+    color: '#506578',
+    active: false
+  },      
+  ]
+}
