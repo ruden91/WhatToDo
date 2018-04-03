f0b9e7f30696054b87beb7868336c2eecb35c408
ruden <ruden91@naver.com>
2018-04-03 23:07:54 +0900

    todoItem style 수정

--- src/components/dashboard/item/TodoItem.tsx
+++ src/components/dashboard/item/TodoItem.tsx
@@ -1,14 +1,39 @@
 import * as React from 'react';
 
 import { updateItem } from 'database/firebase';
+import './TodoItem.scss';
 interface Props {
   content: string;
   uniqueKey: string;
 }
 const TodoItem: React.SFC<Props> = props => (
-  <li>
-    <button onClick={() => updateItem(props.uniqueKey)}>update</button>
-    {props.content}
+  <li className="wtd-dashboard-todo-item">
+    <div className="wtd-dashboard-todo-item__invisible-space">
+      <i className="fas fa-sort" />
+    </div>
+    <table cellPadding="0" cellSpacing="0">
+      <tbody>
+        <tr>
+          <td className="wtd-dashboard-todo-item__checker">
+            <button
+              className="wtd-dashboard-todo-item__check-box"
+              onClick={() => updateItem(props.uniqueKey)}
+            >
+              <i className="fas fa-check" />
+            </button>
+          </td>
+          <td className="wtd-dashboard-todo-item__content">
+            <span>{props.content}</span>
+          </td>
+          <td className="wtd-dashboard-todo-item__project-task" />
+          <td className="wtd-dashboard-todo-item__menu">
+            <button onClick={() => alert('update 추가중')}>
+              <i className="fas fa-ellipsis-h" />
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </li>
 );
 
