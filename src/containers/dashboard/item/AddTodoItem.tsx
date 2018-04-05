ad0543191f2612caf23feac7ffd30600418037a2
ruden <ruden91@naver.com>
2018-04-05 12:52:48 +0900

    addTodoItem 스타일 수정 및 contenteditable 컴포넌트 추가

--- src/containers/dashboard/item/AddTodoItem.tsx
+++ src/containers/dashboard/item/AddTodoItem.tsx
@@ -4,7 +4,8 @@ import InfiniteCalendar from 'react-infinite-calendar';
 import 'react-infinite-calendar/styles.css';
 import * as moment from 'moment';
 import 'moment/locale/ko';
-
+import './AddTodoItem.scss';
+import ContentEditable from 'react-contenteditable';
 interface State {
   content: string;
   selectDate: string | Object;
@@ -21,22 +22,21 @@ export default class AddTodoItem extends React.Component<{}, State> {
     this.setState({ content: e.target.value });
   };
   selectDate = (selectDate: any) => {
-    
     this.setState({
       selectDate: moment(selectDate).format('YYYY-MM-DD'),
       toggleCalendar: false
-    })
-  }
+    });
+  };
   handleCalendar = () => {
     this.setState({
       toggleCalendar: !this.state.toggleCalendar
-    })
-  }
+    });
+  };
 
   handleAddTodoForm = (e: any): void => {
     e.preventDefault();
     const { content, selectDate } = this.state;
-    
+
     if (content === '') {
       return;
     }
@@ -50,159 +50,52 @@ export default class AddTodoItem extends React.Component<{}, State> {
   render() {
     const { toggleCalendar, selectDate } = this.state;
     return (
-      <div>
+      <div className="add-todo-item">
         <form onSubmit={this.handleAddTodoForm}>
-          <input
-            type="text"
-            value={this.state.content}
-            onChange={e => this.changeTodoItemContent(e)}
-          />
-          <button type="submit">add</button>
+          <table className="add-todo-item__input-actions">
+            <tbody>
+              <tr>
+                <td>
+                  <ContentEditable
+                    html={this.state.content} // innerHTML of the editable div
+                    disabled={false} // use true to disable edition
+                    onChange={this.changeTodoItemContent} // handle innerHTML change
+                  />
+                </td>
+                <td>
+                  <button onClick={this.handleCalendar}>
+                    {moment(selectDate).format('YYYY-MM-DD')}
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table className="add-todo-item__actions">
+            <tbody>
+              <tr>
+                <td>
+                  <button type="submit" className="add-todo-item__add-item">
+                    작업 추가
+                  </button>
+                </td>
+                <td>
+                  <a href="javascript:;" className="add-todo-item__cancel-item">
+                    취소
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </form>
-        <button onClick={this.handleCalendar}>calendar toggle</button>
-        {toggleCalendar && <InfiniteCalendar
+        {toggleCalendar && (
+          <InfiniteCalendar
             width={300}
             selected={selectDate}
             height={400}
             onSelect={this.selectDate}
-        />}       
+          />
+        )}
       </div>
     );
   }
 }
-// import React, { Component } from 'react';
-// import * as actions from '../actions';
-// import { database, auth, addTodoItem } from 'database/firebase';
-// import { random, assign } from 'lodash';
-// import moment from 'moment';
-
-// import DayPickerInput from 'react-day-picker/DayPickerInput';
-// import 'react-day-picker/lib/style.css';
-
-// import 'moment/locale/ko';
-
-// // Include the locale utils designed for moment
-// import MomentLocaleUtils, {
-//   formatDate,
-//   parseDate
-// } from 'react-day-picker/moment';
-// export default class AddTodoItem extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = { content: "", openForm: false, due: null };
-//   }
-
-//   changeTodoItemContent = e => {
-//     this.setState({ content: e.target.value });
-//   };
-
-//   createTodoItem = e => {
-//     e.preventDefault();
-//     const { content } = this.state;
-
-//     if (content === "") {
-//       return;
-//     }
-
-//     const uid = auth.currentUser.uid;
-//     const created_at = new Date().getTime();
-//     const due = this.state.due && moment(this.state.due).format('YYYY-MM-DD');
-
-//     addTodoItem({ uid, content, created_at, due });
-
-//     this.setState({ content: "", due: null });
-//   };
-
-//   componentDidUpdate() {
-//     const { openForm } = this.state;
-//     if (typeof this.refs.addTodoItemInput !== "undefined" && openForm) {
-//       this.refs.addTodoItemInput.focus();
-//     }
-//   }
-
-//   CustomOverlay = ({ classNames, selectedDay, children }) => {
-//     return <div className={classNames.overlayWrapper}>
-//         <div className={classNames.overlay}>
-//           {/* <header>
-//             <ul>
-//               <li>
-//                 <button onClick={() => console.log(children)}>
-//                   <span className="fa-layers fa-fw">
-//                     <i className="fas fa-circle-notch" />
-//                     <span className="fa-layers-text fa-inverse" data-fa-transform="shrink-8 down-3">
-//                       19
-//                     </span>
-//                   </span>
-//                 </button>
-//               </li>
-//               <li>
-//                 <button>
-//                   <span className="fa-layers fa-fw">
-//                     <i className="fas fa-calendar" />
-//                     <span className="fa-layers-text fa-inverse" data-fa-transform="shrink-8 down-3">
-//                       27
-//                     </span>
-//                   </span>
-//                 </button>
-//               </li>
-//               <li>
-//                 <button>
-//                   <i className="fas fa-sun" />
-//                 </button>
-//               </li>
-//               <li>
-//                 <button>
-//                   <i className="fas fa-long-arrow-alt-right" data-fa-transform="shrink-5 down-3" data-fa-mask="fas fa-calendar" />
-//                 </button>
-//               </li>
-//               <li>
-//                 <button>
-//                   <i className="fas fa-moon" />
-//                 </button>
-//               </li>
-//               <li>
-//                 <button>
-//                   <i className="far fa-calendar-times" />
-//                 </button>
-//               </li>
-//             </ul>
-//           </header> */}
-//           {children}
-//         </div>
-//       </div>;
-//   };
-
-//   render() {
-//     const { openForm } = this.state;
-//     const { settings, onAddClick } = this.props;
-
-//     return (
-//       <form className="wtd-dashboard__add-todo-item-form" onSubmit={this.createTodoItem}>
-//       <div className="wtd-dashboard__add-todo-wrap">
-//         <input type="text" onChange={this.changeTodoItemContent} value={this.state.content} ref="addTodoItemInput" />
-//         <DayPickerInput
-//           overlayComponent={this.CustomOverlay}
-//           formatDate={formatDate}
-//           parseDate={parseDate}
-//           dayPickerProps={
-//             {
-//               localeUtils: MomentLocaleUtils,
-//               locale: "ko"
-//             }
-//           }
-//           onDayChange={day => this.setState({ due: day })}
-//           // placeholder={this.state.selectedDay}
-//         />
-//       </div>
-
-//       <button type="submit">
-//         작업 추가
-//       </button>
-//       <button onClick={ () => onAddClick() }>
-//         취소
-//       </button>
-//     </form>
-//     )
-//   }
-// }
